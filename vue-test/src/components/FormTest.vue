dbe506e95f9d9347eb19d3c51291353afaeb49ba
wwjhzc <2258660979@qq.com>
2020-01-02 20:57:37 +0800

    Add files via upload

---
+++ vue-test/src/components/FormTest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    form表单
+    <hr />
+    <k-form :model="model" :rules="rules" ref="kForm">
+      <k-form-item label="用户名"  prop="username">
+        <k-input v-model="model.username"></k-input>
+      </k-form-item>
+      <k-form-item label="密码" prop="password">
+        <k-input type="password" v-model="model.password"></k-input>
+      </k-form-item>
+    </k-form>
+  </div>
+</template>
+<script>
+import KForm from "./KForm.vue";
+import KInput from "./KInput.vue";
+import KFormItem from "./KFormItem.vue";
+export default {
+  components: {
+    KForm,
+    KInput,
+    KFormItem
+  },
+  data() {
+    return {
+      // 数据模型
+      model: { username: "tom", password: "" },
+      // 校验规则
+      rules: {
+        username: [{ required: true, message: "请输入用户名" }],
+        password: [{ required: true, message: "请输入密码" }]
+      }
+    };
+  }
+};
+</script>
+<style>
+</style>
---
+++ vue-test/src/components/KFormItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <label v-if="label">{{label}}</label>
+    <!-- 插槽 -->
+    <slot></slot>
+    <!-- 校验错误信息 -->
+    <p v-if="errorMessage" class="error">{{errorMessage}}</p>
+  </div>
+</template>
+
+<script>
+import Validator from "async-validator";
+
+export default {
+    inject: ["form"],
+    props: ["label", "prop"],
+    data() {
+        return {
+            errorMessage: '',
+        }
+    },
+    created() {
+        this.$on("validate", this.validate);
+    },
+    methods: {
+        validate() {
+            console.log('验证')
+            return new Promise(resolve => {
+                // 校验规则制定
+                const descriptor = { [this.prop]: this.form.rules[this.prop] };     // 具体的校验字段，[]为获取括号内的值
+                // 创建校验器
+                const validator = new Validator(descriptor);
+                // 执行校验
+                validator.validate(
+                    {[this.prop]: this.form.model[this.prop]},
+                    errors => {
+                        if(errors) {
+                            // 显示错误信息
+                            this.errorMessage = errors[0].message;
+                            resolve(this);
+                        } else {
+                            this.errorMessage = "";
+                            resolve(this);
+                        }
+                    }
+                )
+            })
+        }
+    }
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
